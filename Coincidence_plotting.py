b1b0d8a55da12951ee6605d6cbf8553cd98f674b
Nicholas Swidinsky <21nes8@queensu.ca>
2024-05-01 13:51:31 -0400

    Initialization of Coincidence_plotting.py

---
+++ Coincidence_plotting.py
@@ -0,0 +1,8 @@
+############################################################################################
+# Code to find coincidences within between different channels of the CAEN DAQ. 
+# 
+# This code iterates upon loadCAEN4.py that was created by Philippe.
+#
+# Created: 2024_05_01
+# Author: Nick Swidinsky
+###########################################################################################
