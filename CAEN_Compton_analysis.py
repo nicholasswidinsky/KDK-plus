6bc59797d56359000941a86d0ce72ffc2613936d
Nicholas Swidinsky <21nes8@queensu.ca>
2025-07-17 16:24:36 -0400

    Updates to 2D histogram plotting. Still needs final tweaking

--- CAEN_Compton_analysis.py
+++ CAEN_Compton_analysis.py
@@ -4,8 +4,9 @@ import matplotlib
 from pathlib import Path
 import statistics
 import math
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
-matplotlib.rcParams["font.size"] = 15
+matplotlib.rcParams["font.size"] = 20
 matplotlib.rcParams["lines.linewidth"] = 3
 matplotlib.rcParams["mathtext.default"] = 'regular'
 matplotlib.rcParams['lines.markersize'] = 3
@@ -14,6 +15,17 @@ LeftPMTScale = 373.609/2104.961738528885 #Scale factors for hte two PMTs so that
 RightPMTScale = 373.609/1668.61105108102
 
 scalefactor = [LeftPMTScale,RightPMTScale]
+
+detectors = {
+    "Channel 0" : "LS Left PMT",
+    "Channel 2" : "LS RIght PMT",
+    "Channel 4" : "LS Left PMT",
+    "Channel 6" : "NaI",
+    "Channel 8" : "NaI 1",
+    "Channel 10" : "NaI 2",
+    "Channel 12" : "NaI 3",
+    "Channel 14" : "NaI 4",
+}
 
 
 def BinHistograms(data, Range, numBins):
@@ -120,13 +132,13 @@ def energyHist1D(data, Bckdata, ChBins, BinRange, norm, log,saveFilePath,fileNam
         for i in range(len(data)):
             Int, Bins = BinHistograms(data[i][1],BinRange[i],ChBins[1])
 
-            ax[i].hist(Bins[:-1],bins =Bins,density = norm,weights=Int/(data[i][0][-1] - data[i][0][0]),histtype = 'step',label = f'Ch {Channel[i]}', log = log)
+            ax[i].hist(Bins[:-1],bins =Bins,density = norm,weights=Int/(data[i][0][-1] - data[i][0][0]),histtype = 'step',label = detectors[f'Channel {Channel[i]}'], log = log)
         
             if bck:
                 bckInt, bckBins = BinHistograms(Bckdata[i][1],BinRange[i],ChBins[1])
                 ax[i].hist(bckBins[:-1],bins =bckBins,density = norm,weights=bckInt/(Bckdata[i][0][-1] - Bckdata[i][0][0]),histtype = 'step',label = f'Background Ch {Channel[i]} ', log = log)
 
-            ax[i].set_title(f'Channel {Channel[i]}')
+            ax[i].set_title(detectors[f'Channel {Channel[i]}'])
             ax[i].set_xlabel('Integral (ADC Channel)')
             ax[i].set_ylabel('Counts/bin/ps')
             ax[i].legend(loc = 'best')
@@ -139,14 +151,15 @@ def energyHist1D(data, Bckdata, ChBins, BinRange, norm, log,saveFilePath,fileNam
         for i in range(len(data)):
             Int, Bins = BinHistograms(data[i][1],BinRange[i],ChBins[1])
 
-            ax[row,col].hist(Bins[:-1],bins =Bins,density = norm,weights=Int/(data[i][0][-1] - data[i][0][0]),histtype = 'step',label = f'Ch {Channel[i]}', log = log)
+            ax[row,col].hist(Bins[:-1],bins =Bins,density = norm,weights=Int/(data[i][0][-1] - data[i][0][0]),histtype = 'step',label =detectors[f'Channel {Channel[i]}'], log = log)
             
             
             if bck:
                 bckInt, bckBins = BinHistograms(Bckdata[i][1],BinRange[i],ChBins[1])
                 ax[row,col].hist(bckBins[:-1],bins =bckBins,density = norm,weights=bckInt/(Bckdata[i][0][-1] - Bckdata[i][0][0]),histtype = 'step',label = f'Background Ch {Channel[i]} ', log = log)
 
-            ax[row,col].set_title(f'Channel {Channel[i]}')
+            ax[row,col].set_title(detectors[f'Channel {Channel[i]}'])
+            
             if scale:
                 ax[row,col].set_xlabel('Integral (KeV)')
             else:
@@ -178,7 +191,7 @@ def Stability_plots(data, ChBins,saveFilePath,fileName,Channel,scale):
         timebinsRange = np.linspace(min(data[i][0]),max(data[i][0]),100)
         
         ax[0,i].hist(data[i][1],bins = energybinsRange,histtype = 'step')
-        ax[0,i].set_title(f'Channel {Channel[i]}')
+        ax[0,i].set_title(detectors[f'Channel {Channel[i]}'])
         if scale:
             ax[0,i].set_xlabel('Integral (KeV)')
         else:
@@ -187,7 +200,7 @@ def Stability_plots(data, ChBins,saveFilePath,fileName,Channel,scale):
         
         h=ax[1,i].hist2d(data[i][0],data[i][1], bins = (timebinsRange,energybinsRange))
         fig.colorbar(h[3], ax=ax[1, i])
-        ax[1,i].set_title(f'Channel {Channel[i]}')
+        ax[1,i].set_title(detectors[f'Channel {Channel[i]}'])
         ax[1,i].set_xlabel('Trigger Time (s)')
         
         ax[1,i].set_ylabel('Integral (ADC Channel)')
@@ -237,29 +250,45 @@ def energyHist2D(data,ChBins,BinRange,saveFilePath,fileName,Channels,log,scale):
     
     
     
-    fig,ax = plt.subplots(len(data),len(data),figsize = (10*len(data),10*len(data)))    
-    
+    fig,ax = plt.subplots(len(data),len(data),figsize = (10*len(data),10*len(data)))   
+    plt.tight_layout() 
+    plt.subplots_adjust(wspace=0.4)
+
     for i in range(len(data)):
         for j in range(len(data)):
             
+            if j < i:
+                ax[i,j].set_axis_off()
+                continue
+            # ax[i, j].set_aspect('equal')
             if log:
                 h=ax[i,j].hist2d(data[i][1],data[j][1],bins = (ChBinsArray[i],ChBinsArray[j]),cmin = 1,norm=matplotlib.colors.LogNorm())
             else:
                 h=ax[i,j].hist2d(data[i][1],data[j][1],bins = (ChBinsArray[i],ChBinsArray[j]),cmin = 1)
                 
             fig.colorbar(h[3], ax=ax[i, j])
+            # cb_ax = inset_axes(ax[i, j],
+            #         width="5%", height="100%",  # narrow vertical bar
+            #         loc='right',
+            #         bbox_to_anchor=(0.07, 0., 1, 1),
+            #         bbox_transform=ax[i, j].transAxes,
+            #         borderpad=0)
+            # fig.colorbar(h[3], cax=cb_ax)
+
+            
             if i == 1 and j == 0:
                 ax[i,j].plot(data[i][1],data[i][1], color = 'red', linestyle = '-.')
             elif i == 0 and j == 1:
                 ax[i,j].plot(data[i][1],data[i][1], color = 'red', linestyle = '-.')
                 
-            ax[i,j].set_title(f'Ch{Channels[i]}, Ch{Channels[j]} 2D hist')
+            # ax[i,j].set_title(f'Ch{Channels[i]}, Ch{Channels[j]} 2D hist')
+            ax[i,j].set_title(f'{detectors[f'Channel {Channels[i]}']}, {detectors[f'Channel {Channels[j]}']} 2D hist')
             if scale:
-                ax[i,j].set_xlabel(f'Ch{Channels[i]} Integral (KeV)')
-                ax[i,j].set_ylabel(f'Ch{Channels[j]} Integral (KeV)')
+                ax[i,j].set_xlabel(f'{detectors[f'Channel {Channels[i]}']} Integral (KeV)')
+                ax[i,j].set_ylabel(f'{detectors[f'Channel {Channels[j]}']} Integral (KeV)')
             else: 
-                ax[i,j].set_xlabel(f'Ch{Channels[i]} Integral (ADC Channel)')
-                ax[i,j].set_ylabel(f'Ch{Channels[j]} Integral (ADC Channel)')
+                ax[i,j].set_xlabel(f'{detectors[f'Channel {Channels[i]}']} Integral (ADC Channel)')
+                ax[i,j].set_ylabel(f'{detectors[f'Channel {Channels[j]}']} Integral (ADC Channel)')
                 
     if scale:
         plt.savefig(f'{saveFilePath}/{fileName}_scaled_integral_2D_hist.png',bbox_inches='tight')
@@ -304,53 +333,53 @@ def TimeDiff2D(data,ChBins,ChBinRange,TimeBinRange,saveFilePath,fileName,Channel
         
 
 def TimeCut(data,timeCut,CoincCH,Channels):
-    ######################################################################################################
-    # Data: N-dimensional array that contains three more arrays per element. These sub arrays hold the information for each event. The N-dimensions of the total array correspond to individual channels. 
-    # 
-    # timeCut: array with two elements that hold the start and the end of the timecut. 
-    #
-    # CoincCH: Array with two elements that corresponds to the two channels that will be compared for the time cut. 
-    #
-    # Channels: Array of all the channels used in the experiment. This is done purely to coordinate the indeces for data to the CoincCH values. 
-    ######################################################################################################
+#     ######################################################################################################
+#     # Data: N-dimensional array that contains three more arrays per element. These sub arrays hold the information for each event. The N-dimensions of the total array correspond to individual channels. 
+#     # 
+#     # timeCut: array with two elements that hold the start and the end of the timecut. 
+#     #
+#     # CoincCH: Array with two elements that corresponds to the two channels that will be compared for the time cut. 
+#     #
+#     # Channels: Array of all the channels used in the experiment. This is done purely to coordinate the indeces for data to the CoincCH values. 
+#     ######################################################################################################
     
-    Channels = np.asfarray(Channels, int)
-    CoincCh = np.asfarray(CoincCh, int)
+#     Channels = np.asfarray(Channels, int)
+#     CoincCh = np.asfarray(CoincCh, int)
 
-    Channelind = np.where((Channels >= sorted(CoincCh)[0]) & (Channels <= sorted(CoincCh)[1]))
+#     Channelind = np.where((Channels >= sorted(CoincCh)[0]) & (Channels <= sorted(CoincCh)[1]))
     
-    timeDiff = TimeDifferenceChannel(data,Channelind)
+#     timeDiff = TimeDifferenceChannel(data,Channelind)
     
-    CoincData = [[],[],[]]
-    cutData = [[],[],[]]
-    for i in range(len(data[0][0])):
-        if timeDiff[i] > timeCut[0] and timeDiff[i] < timeCut[1]:
-            CoincData[0].append(data[0][1][i])
-            CoincData[1].append(data[1][1][i])
-            CoincData[2].append(data[2][1][i])
-        else:
-            cutData[0].append(data[0][1][i])
-            cutData[1].append(data[1][1][i])    
-            cutData[2].append(data[2][1][i])    
+#     CoincData = [[],[],[]]
+#     cutData = [[],[],[]]
+#     for i in range(len(data[0][0])):
+#         if timeDiff[i] > timeCut[0] and timeDiff[i] < timeCut[1]:
+#             CoincData[0].append(data[0][1][i])
+#             CoincData[1].append(data[1][1][i])
+#             CoincData[2].append(data[2][1][i])
+#         else:
+#             cutData[0].append(data[0][1][i])
+#             cutData[1].append(data[1][1][i])    
+#             cutData[2].append(data[2][1][i])    
             
     
-    CoincData[0] = np.asfarray(CoincData[0],float)
-    CoincData[1] = np.asfarray(CoincData[1],float)
-    CoincData[2] = np.asfarray(CoincData[2],float)
+#     CoincData[0] = np.asfarray(CoincData[0],float)
+#     CoincData[1] = np.asfarray(CoincData[1],float)
+#     CoincData[2] = np.asfarray(CoincData[2],float)
     
-    cutData[0] = np.asfarray(cutData[0],float)
-    cutData[1] = np.asfarray(cutData[1],float)
-    cutData[2] = np.asfarray(cutData[2],float)
+#     cutData[0] = np.asfarray(cutData[0],float)
+#     cutData[1] = np.asfarray(cutData[1],float)
+#     cutData[2] = np.asfarray(cutData[2],float)
     
-    # CoincData = np.asfarray(CoincData,float)
-    # cutData = np.asfarray(cutData,float)
+#     # CoincData = np.asfarray(CoincData,float)
+#     # cutData = np.asfarray(cutData,float)
     
-    return CoincData, cutData
+#     return CoincData, cutData
                 
 
         
     
-def TimeCutHist(data,ChBins,ChBinRange,norm,TimeBinRange,timeCut,saveFilePath,fileName):
+# def TimeCutHist(data,ChBins,ChBinRange,norm,TimeBinRange,timeCut,saveFilePath,fileName):
     
     CoincData, cutData = TimeCut(data,timeCut)
     # print(type(CoincData))
@@ -410,12 +439,53 @@ def TimeCutHist(data,ChBins,ChBinRange,norm,TimeBinRange,timeCut,saveFilePath,fi
             
     plt.savefig(f'{saveFilePath}/{fileName}_time_hist_with_cuts.png',bbox_inches='tight')
     
+def CutDataRange(data,cutrange,cutCh,cutVariable,Channels):
+    ################################################
+    # Function that cuts all the data around a     #
+    # value. For example, you can decide to cut    #
+    # on energy by specifying the range in energy  #
+    # to cut.                                      #
+    ################################################
+    # Variables:                                   #
+    # Data: Array. Contains all the information    #
+    #       each detector in its own sub array.    #
+    #       Each detector follows the same format: #
+    #       [[time][energy][flags]]                #
+    # cutrange: Array. array of two floats, or ints#
+    #           
+    
+    cutData = []
     
     
-filepath = '/home/nick/PhD/KDK+/Large_LSC_testing/Position_tests/2025_05_01/'
+    if cutVariable == "Energy":
+        cutVarInd = 1
+    elif cutVariable == "Time":
+        cutVarInd = 0
+        
+    # for c in range(len(Channels)):
+    for c in range(len(Channels)):
+        cutData.append([ [] for _ in range(3)])
+        
+    for i in range(len(data[0][0])):
+        if data[cutCh][cutVarInd][i] >= cutrange[0] and data[cutCh][cutVarInd][i] <= cutrange[1]:
+            cutData[0][0].append(data[0][0][i])
+            cutData[0][1].append(data[0][1][i])
+            cutData[0][2].append(data[0][2][i])
+            
+            cutData[1][0].append(data[1][0][i])
+            cutData[1][1].append(data[1][1][i])
+            cutData[1][2].append(data[1][2][i])
+            
+            cutData[2][0].append(data[2][0][i])
+            cutData[2][1].append(data[2][1][i])
+            cutData[2][2].append(data[2][2][i])
+            
+    return cutData    
+    
+filepath = '/home/nick/PhD/KDK+/Large_LSC_testing/Position_tests_outer_sleeve/2025_07_17/Large_LSC_vessel_Cs137_triple_coinc_Vertical_scatter_v4_central_position/RAW/'
 
-GammaDetector = 'NaI'
-file = 'SDataR_Large_LSC_vessel_Cs137_triple_coinc_Vertical_scatter_v4_position_3_10fc_corrected.CSV'
+# GammaDetector = 'NaI'
+file = 'SDataR_Large_LSC_vessel_Cs137_triple_coinc_Vertical_scatter_v4_central_position_corrected.CSV'
 bckfile = '/home/nick/PhD/Plastic_scintillators/Cryostat_gamma_V2/Darkbox_tests/CAEN_DAQ/2025_05_13/SDataR_Small_PSC_Darkbox_NaI_bck_coinc_2.CSV'
 
 backgrounds = False
@@ -434,10 +504,10 @@ timeBinRange = [-500000,500000]
 integralBinRange = [[0] for _ in range(len(Channels))]
 
 
-for i in range(len(Channels)):
-    integralBinRange[i].append(min(math.ceil(statistics.median(dataFile[i][1])/100)*200,4000))
-print(integralBinRange)
-
+# for i in range(len(Channels)):
+#     integralBinRange[i].append(min(math.ceil(statistics.median(dataFile[i][1])/1000)*1000,4000))
+# # print(integralBinRange)
+integralBinRange = [[0,4000],[0,4000],[0,4000]]
 
 fileName = file.split('.CSV')[0]
 bckfileName = bckfile.split('/')[-1].split('.CSV')[0]
@@ -455,6 +525,12 @@ energyHist2D(dataFile, ChBins = [integralBins for _ in range(len(Channels))], Bi
 TimeDiff2D(data = dataFile,ChBins = [integralBins for _ in range(len(Channels))],ChBinRange= integralBinRange,TimeBinRange= timeBinRange,saveFilePath = saveFilePath,fileName = fileName, Channels = Channels, log = Log,scale = scale)
 # except:
 #     pass
+
+# energyCut = CutDataRange(data = dataFile,cutrange = [500,1000],cutCh=2,cutVariable='Energy',Channels = Channels)
+# # print(energyCut)
+# energyHist1D(data = energyCut, Bckdata = bckdataFile, ChBins = [integralBins for _ in range(len(Channels))], BinRange = integralBinRange, norm = normalize, log = Log,saveFilePath = saveFilePath,fileName = f"{fileName}_energy_cut", Channel = Channels, bck = backgrounds,scale = scale)
+# energyHist2D(energyCut, ChBins = [integralBins for _ in range(len(Channels))], BinRange = integralBinRange,saveFilePath = saveFilePath,fileName = f"{fileName}_energy_cut",Channels = Channels,log = Log,scale = scale)
+# TimeDiff2D(data = energyCut,ChBins = [integralBins for _ in range(len(Channels))],ChBinRange= integralBinRange,TimeBinRange= timeBinRange,saveFilePath = saveFilePath,fileName = f"{fileName}_energy_cut", Channels = Channels, log = Log,scale = scale)
 
 # energyHist2D(bckdataFile, ChBins = [100,100,100,100], BinRange = [[0,4000],[0,4000],[0,4000],[0,4000]],saveFilePath = saveFilePath,fileName = bckfileName, Channels = [0,2,6,8])
 # TimDiff2D(data = bckdataFile,ChBins = [100,100,100,100],ChBinRange= [[0,4000],[0,4000],[0,4000],[0,4000]],TimeBinRange= [-400000,400000],saveFilePath = saveFilePath,fileName = bckfileName)
