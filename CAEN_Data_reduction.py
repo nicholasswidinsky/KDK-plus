8c5c55c2463f34548dda856e91c5dd9bdb1ba777
Nicholas Swidinsky <21nes8@queensu.ca>
2026-05-14 10:59:44 -0400

    Updated reduction to remove waveforms

--- CAEN_Data_reduction.py
+++ CAEN_Data_reduction.py
@@ -35,6 +35,9 @@ class CoicidenceData:
     def writeToDisk(self,filepath,filename):
         
         sortedCh = sorted(self.ch)
+        self.t = [x for _,x in sorted(zip(self.ch,self.t),key=lambda pair:pair[0])]
+        self.data = [x for _,x in sorted(zip(self.ch,self.data),key=lambda pair:pair[0])]
+        self.ch = sortedCh
         
         coincChannelsStr = '_'.join([str(i) for i in sortedCh])
         
@@ -46,80 +49,49 @@ class CoicidenceData:
             coincList.append(sortedCh)
             openedfiles.append(coincfile)
             f = open(coincfile,"w+")
-            f.write(f"Data reduction code output.\nEach line saves the information for every channel in the coincidence. Note that currently waveforms are not saved in this data reduction. \nEach channel has the following headers: BOARD;CHANNEL;TIMETAG;ENERGY;ENERGYSHORT;FLAGS;PROBE_CODE;SAMPLES\nNumber of channels in this coincidenc: {len(self.ch)}\n")
+            f.write(f"Data reduction code output.\nEach line saves the information for every channel in the coincidence. Note that currently waveforms are not saved in this data reduction. \nEach channel has the following headers: BOARD;CHANNEL;TIMETAG;ENERGY;ENERGYSHORT;FLAGS;PROBE_CODE;SAMPLES\nNumber of channels in this coincidence: {len(self.ch)}\n")
         
         f.write(';'.join(self.data) + '\n')
         f.close()
         
-        
-
-def ReadInData(filepath,filename,savefilepath,coincWindow,):
-    # coinc_list = []
-    # current_coinc = Coincidences()
-    # first_event_time = None
+     
+def ReadInData(filepath, filename, savefilepath, coincWindow):
     counter = 0
 
     with open(f"{filepath}/{filename}.CSV") as f:
-        next(f)
+        next(f)  # skip header
 
         for i, line in enumerate(f):
+            data = line.strip().split(';')
             
-            lines = line.split('\n')[0]
-            # print(lines)
+            # Only keep the first 6 fields, ignoring waveform samples beyond that
+            cleanLine = ';'.join(data[:6])
+
+            t  = int(data[2]) / 1e3
+            ch = int(data[1])
+
             if counter == 0:
-               
-                data = line.strip().split(';')
-
-                t = int(data[2]) / 1e3
-                ch = int(data[1])
-
-                coinc = CoicidenceData(ch,t,lines)
-            
-            else: 
-                data = line.strip().split(';')
-
-                t = int(data[2]) / 1e3
-                ch = int(data[1])
-                
+                coinc = CoicidenceData(ch, t, cleanLine)
+            else:
                 dt = np.absolute(coinc.t[0] - t)
-                
+
                 if dt <= coincWindow:
-                    coinc.addEvent(ch,t,lines)
-                    
+                    coinc.addEvent(ch, t, cleanLine)
                 else:
-                    coinc.writeToDisk(filepath=savefilepath,filename=filename)
-                    
-                    coinc = CoicidenceData(ch,t,lines)
-            counter += 1 
+                    coinc.writeToDisk(filepath=savefilepath, filename=filename)
+                    coinc = CoicidenceData(ch, t, cleanLine)
+
+            counter += 1
             if counter % 100000 == 0:
-                print(f'{counter} lines sorted')
-            # counter += 1
-            # # if counter % 100000 == 0:
-            # #     print(f"Read in {counter} events")
-
-            # if first_event_time is None:
-            #     first_event_time = t
-
-            # dt = abs(first_event_time - t)
-
-            # if dt > CoincWindow:
-            #     if current_coinc.Channels not in CoincChannels:
-            #         del current_coinc
-            #     else:
-            #         coinc_list.append(current_coinc)
-            #     current_coinc = Coincidence()
-            #     first_event_time = t
-
-            # current_coinc.AddEvent(evt, ch)
-
-        # coinc_list.append(current_coinc)
+                print(f'{counter} lines sorted')        
+                
 
 
-file = '/home/nick/PhD/KDK+/Annulus_Compton_scatter_V1/2026_01_28/NaI_annulus_LS_2_Cs137_All_NaI_higher_HV/RAW/SDataR_NaI_annulus_LS_2_Cs137_All_NaI_higher_HV.CSV'
+file = Path('/home/nick/PhD/KDK+/Daily_LSC_Calibration_testing/2026_05_05/2026_05_05_Daily_LSC_calibration_Cs137_coinc/RAW/SDataR_2026_05_05_Daily_LSC_calibration_Cs137_coinc.CSV')
 # filename = 'SDataR_NaI_annulus_LS_Cs137_NaI_1_2_3_4_triple_coinc.CSV'
 
-filename = file.split('.CSV')[0].split('/')[-1]
-filepath = file.split('.CSV')[0].split(filename)[0]
+filename = str(file).split('.CSV')[0].split('/')[-1]
+filepath = str(file).split('.CSV')[0].split(filename)[0]
 
 savefilepath = f"{filepath}/coinc_sorted"
 Path(savefilepath).mkdir(parents=True, exist_ok = True)
