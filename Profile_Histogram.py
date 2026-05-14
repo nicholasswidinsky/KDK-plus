e2e9346c84db412eb99bcde2059ef9f0b8420152
Nicholas Swidinsky <21nes8@queensu.ca>
2026-05-14 10:03:27 -0400

    Profile Histogram daily Calibration

--- Profile_Histogram.py
+++ Profile_Histogram.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import xml.etree.ElementTree as ET
 from pathlib import Path
 import scipy.stats as Stats
-from ROOT import TCanvas, TH2D, TCutG,TProfile, TF1, kRed,TLegend
+import re
+from ROOT import TCanvas, TH2D, TCutG,TProfile, TF1, kRed,TLegend,TMath
 
 
 class ChData:
@@ -45,8 +46,8 @@ class Hists2D:
         self.cut = None  
         self.fit = None      
         
-    def Draw2DHist(self):
-        self.canvas = TCanvas()
+    def Draw2DHist(self,saveFilePath):
+        self.canvas = TCanvas(f"canvas_ch{self.ch[0]}_vs_ch{self.ch[1]}", "canvas", 800, 600)
         
         self.hist2D.GetXaxis().SetTitle(f"{channels[f'Channel {self.ch[0]}'][0]} Energy (ADU)")
         self.hist2D.GetYaxis().SetTitle(f"{channels[f'Channel {self.ch[1]}'][0]} Energy (ADU)")
@@ -63,22 +64,22 @@ class Hists2D:
             self.fit.Draw("same")
             
             # Extract fit results
-            p0       = self.fit.GetParameter(0)
-            p0_err   = self.fit.GetParError(0)
-            p1       = self.fit.GetParameter(1)
-            p1_err   = self.fit.GetParError(1)
-            chi2     = self.fit.GetChisquare()
-            ndof     = self.fit.GetNDF()
-            p_value  = Stats.chi2.sf(chi2, ndof)  # survival function = 1 - CDF
+            self.p0       = self.fit.GetParameter(0)
+            self.p0_err   = self.fit.GetParError(0)
+            self.p1       = self.fit.GetParameter(1)
+            self.p1_err   = self.fit.GetParError(1)
+            self.chi2     = self.fit.GetChisquare()
+            self.ndof     = self.fit.GetNDF()
+            self.p_value  = Stats.chi2.sf(self.chi2, self.ndof)  # survival function = 1 - CDF
             
             # Build legend — positioned to the left of the colour bar
             # (x1, y1, x2, y2) in NDC coordinates (0-1)
             legend = TLegend(0.12, 0.65, 0.55, 0.88)
             legend.SetHeader("Fit Results (pol1)", "C")
-            legend.AddEntry(self.fit, f"p0 = {p0:.2f} #pm {p0_err:.2f}", "l")
-            legend.AddEntry((0), f"p1 = {p1:.4f} #pm {p1_err:.4f}", "")
-            legend.AddEntry((0), f"#chi^{{2}} / ndof = {chi2:.2f} / {ndof}", "")
-            legend.AddEntry((0), f"p-value = {p_value:.4f}", "")
+            legend.AddEntry(self.fit, f"p0 = {self.p0:.2f} #pm {self.p0_err:.2f}", "l")
+            legend.AddEntry((0), f"p1 = {self.p1:.4f} #pm {self.p1_err:.4f}", "")
+            legend.AddEntry((0), f"#chi^{{2}} / ndof = {self.chi2:.2f} / {self.ndof}", "")
+            legend.AddEntry((0), f"p-value = {self.p_value:.4f}", "")
             legend.SetBorderSize(1)
             legend.SetFillColorAlpha(0, 0.6)  # semi-transparent background
             self.legend = legend               # store to prevent garbage collection
@@ -87,6 +88,25 @@ class Hists2D:
             
         
         self.canvas.Update()
+        self.canvas.SaveAs(str(saveFilePath / f"Ch_{self.ch[0]}_vs_Ch_{self.ch[1]}_profile_histogram.png"))
+        self.canvas.SaveAs(str(saveFilePath / f"Ch_{self.ch[0]}_vs_Ch_{self.ch[1]}_profile_histogram.pdf"))
+        
+    def saveData(self,filepath, outputFileName):
+        date = filepath.parent.parent.parent.parent.stem
+        
+        outputFile = filepath / f"{outputFileName}_Profile_hist_fit_results.txt"
+        
+        writeHeader = not outputFile.exists()
+        
+        with open(outputFile, "a") as f:
+            if writeHeader:
+                f.write('Date,ch1,ch2,fit func, fit range, chi^2, nDOF, P-value, P0, P0Err, P1, P1Err \n')
+                
+        # f = open(filepath / f"Ch_{self.ch[0]}_vs_Ch_{self.ch[1]}_profile_histogram_fit_results.txt", "w+")
+        # f.write('Date,fit func, fit range, chi^2, nDOF, P-value, P0, P0Err, P1, P1Err \n')
+            f.write(f'{date}, {self.ch[0]}, {self.ch[1]},Pol1, [{self.fitRange[0]}_{self.fitRange[1]}], {self.chi2},{self.ndof},{self.p_value},{self.p0},{self.p0_err},{self.p1},{self.p1_err}\n')
+        
+        f.close()
         
     def cutHist(self,cutEndPoints):
         self.cut = TCutG(f'Banana_cut_{channels[f'Channel {self.ch[0]}'][0]}_vs_{channels[f'Channel {self.ch[1]}'][0]}', 4)
@@ -95,8 +115,16 @@ class Hists2D:
         self.cut.SetLineColor(kRed+2)
         self.cut.SetLineWidth(3)
         
-    def makeProfileHist(self,fitRange):
+    def makeProfileHist(self,scaleFactor):
+
         self.profile = TProfile(f"profile_ch_{channels[f'Channel {self.ch[0]}'][0]}_vs_ch_{channels[f'Channel {self.ch[1]}'][0]}",f"profile Histogram {channels[f'Channel {self.ch[0]}'][0]} vs {channels[f'Channel {self.ch[1]}'][0]}", 100,0,4000,0,4000,"")
+        
+        self.cutHist2D = TH2D( f"cut_hist_ch_{self.ch[0]}_vs_ch_{self.ch[1]}",
+        f"Cut Histogram Ch {self.ch[0]} vs Ch {self.ch[1]}",
+        100, 0, 4000, 100, 0, 4000)
+        
+        
+        maxBin = 0
         for bini in range(1, self.hist2D.GetNbinsX() + 1):
             for binj in range(1, self.hist2D.GetNbinsY() + 1): 
                 x = self.hist2D.GetXaxis().GetBinCenter(bini)
@@ -104,10 +132,50 @@ class Hists2D:
                 z = self.hist2D.GetBinContent(bini,binj)
                 
                 if not self.cut.IsInside(x,y) and z > 0:
+                    # self.profile.Fill(x,y,z)
+                    if z > maxBin:
+                        maxBin = z
+        
+        # cutThreshold = 0.5*np.sqrt(maxBin)
+        cutThreshold = scaleFactor*maxBin
+        
+        for bini in range(1, self.hist2D.GetNbinsX() + 1):
+            for binj in range(1, self.hist2D.GetNbinsY() + 1): 
+                x = self.hist2D.GetXaxis().GetBinCenter(bini)
+                y = self.hist2D.GetYaxis().GetBinCenter(binj)
+                z = self.hist2D.GetBinContent(bini,binj)
+                
+                if not self.cut.IsInside(x,y) and z > cutThreshold:
                     self.profile.Fill(x,y,z)
-        self.fit = TF1(f"fit_{channels[f'Channel {self.ch[0]}'][0]}_vs_{channels[f'Channel {self.ch[1]}'][0]}","pol1",fitRange[0], fitRange[1])
+                    self.cutHist2D.Fill(x,y,z)
+        
+        xBins = []
+        for bini in range(1, self.cutHist2D.GetNbinsX() + 1):
+            for binj in range(1, self.cutHist2D.GetNbinsY() + 1): 
+                z = self.cutHist2D.GetBinContent(bini,binj)
+                if z > 0:
+                    xBins.append(self.cutHist2D.GetXaxis().GetBinCenter(bini))
+            
+        self.fitRange = [min(xBins),max(xBins)]
+  
+             
+        self.fit = TF1(f"fit_{channels[f'Channel {self.ch[0]}'][0]}_vs_{channels[f'Channel {self.ch[1]}'][0]}","pol1",self.fitRange[0], self.fitRange[1])
         self.profile.Fit(self.fit, "RN")
         self.fit.SetLineColor(kRed)
+        
+        self.cutCanvas = TCanvas(
+            f"cut_canvas_ch{self.ch[0]}_vs_ch{self.ch[1]}",
+            f"Cut Verification Ch {self.ch[0]} vs Ch {self.ch[1]}",
+            1600, 1200
+        )
+        self.cutHist2D.SetStats(0)
+        self.cutHist2D.GetXaxis().SetTitle(f"Channel {self.ch[0]} Energy (keV)")
+        self.cutHist2D.GetYaxis().SetTitle(f"Channel {self.ch[1]} Energy (keV)")
+        self.cutHist2D.GetZaxis().SetTitle("Counts")
+        self.cutHist2D.Draw("COLZ")
+        self.cutCanvas.Update()
+        
+
                 
                 
 def readInData(file):
@@ -193,30 +261,51 @@ def ReadInChannelNames(settings):
     return channels
     
     
-# filePath = '/home/nick/PhD/KDK+/Annulus_Compton_scatter_V1/2026_01_28/NaI_annulus_LS_2_Cs137_All_NaI_higher_HV/RAW/coinc_sorted/SDataR_NaI_annulus_LS_2_Cs137_All_NaI_higher_HV_coinc_4_5_8.txt'
+rootfilePath = Path('/home/nick/PhD/KDK+/Daily_LSC_Calibration_testing/2026_04_30/2026_04_30_Daily_LSC_calibration_Cs137_coinc/RAW/coinc_sorted') #filepath to the coinc sorted directory. 
 
-filePath = Path('/home/nick/PhD/KDK+/Daily_LSC_Calibration_testing/2026_04_30/2026_04_30_Daily_LSC_calibration_Cs137_coinc/RAW/coinc_sorted/SDataR_2026_04_30_Daily_LSC_calibration_Cs137_coinc_coinc_4_5_8.txt')
+NaIChannels = ['8','10','12','14'] #Protects against the possibility of having a werid channel coincidence layout with incorrect channel numbers. 
 
-settingsFilePath = filePath.parent.parent.parent / 'settings.xml'
+pattern = re.compile(r'_coinc_4_5_(8|10|12|14)\.txt$')
 
-#Read in the detector names from the settings file. 
-channels = ReadInChannelNames(settingsFilePath)
-                
-cData = readInData(filePath)
+coincFiles = sorted([
+    f for f in rootfilePath.glob('*_coinc_4_5_*.txt')
+    if pattern.search(f.name)
+])
 
-hist2DData = make2DHists(cData)
+for filePath in coincFiles:
+    settingsFilePath = filePath.parent.parent.parent / 'settings.xml'
+    saveFilePath = filePath.parent.parent / 'Daily_calibration_fits' / 'figures'
+    outputFileName = filePath.stem
+    outputFile = saveFilePath / f"{outputFileName}_Profile_hist_fit_results.txt"
+    
+    if outputFile.exists():
+        outputFile.unlink()
 
-cutEndPoints = [[0,0],
-                [0,1000],
-                [1600,0],
-                [0,0]]
+    saveFilePath.mkdir(parents=True, exist_ok=True)
 
-fitRange = [200,1100]
+    #Read in the detector names from the settings file. 
+    channels = ReadInChannelNames(settingsFilePath)
+                    
+    cData = readInData(filePath)
 
-for data in hist2DData:
-    data.cutHist(cutEndPoints)
-    data.makeProfileHist(fitRange)
-    data.Draw2DHist()
+    hist2DData = make2DHists(cData)
+
+    cutEndPoints = [[0,0],
+                    [0,4000],
+                    [50,4000],
+                    [50,800],
+                    [800,50],
+                    [4000,50],
+                    [4000,0],
+                    [0,0]]
+
+    scaleFactor = 0.6 #With low statistics this number needs to be reduced. With high statistics you can keep this around 0.6.
+    for data in hist2DData:
+        data.cutHist(cutEndPoints)
+        data.makeProfileHist(scaleFactor)
+        data.Draw2DHist(saveFilePath)
+        data.saveData(saveFilePath,outputFileName)
+    
 
 # test2DHist = Hists2D([cData.chData['4'],cData.chData['8']],[cData.chData['4'].E,cData.chData['8'].E])
 # test2DHist.Draw2DHist()
